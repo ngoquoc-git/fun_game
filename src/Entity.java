f4f49533f34b3c01ab2436af17fef567a77e7513
quocngo-git <ngoquoc.work@gmail.com>
2021-03-30 15:40:42 -0700

    update Entity
    Constructor, get functions

--- src/Entity.java
+++ src/Entity.java
@@ -1,3 +1,25 @@
 public abstract class Entity {
     
+    //An entity basic components
+    private String name;
+    private int maxHP;
+    private int hp;
+
+    //Create an entity with a name and max HP
+    public Entity(String name, int mHP){
+        this.name = name;
+        this.maxHP = mHP;
+    }
+
+    //Abstract attack action that will be used by Heroes and Enemies
+    abstract String attack(Entity e);
+
+    //Get entity's name
+    String getName(){return name;}
+    
+    //Get entity's current health points
+    int getHP(){return hp;}
+
+    //Get entity's meximum health points
+    int getMaxHP(){return maxHP;}
 }

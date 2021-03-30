338d5a5890e4c4a76aad804f57ebc0e7ceb882a6
quocngo-git <ngoquoc.work@gmail.com>
2021-03-30 16:09:45 -0700

    Update Entity class
    heal, takeDamage, toString

--- src/Entity.java
+++ src/Entity.java
@@ -9,6 +9,7 @@ public abstract class Entity {
     public Entity(String name, int mHP){
         this.name = name;
         this.maxHP = mHP;
+        this.hp = mHP;
     }
 
     //Abstract attack action that will be used by Heroes and Enemies
@@ -23,5 +24,17 @@ public abstract class Entity {
     //Get entity's meximum health points
     public int getMaxHP(){return maxHP;}
 
+    //Heal an entity h health point
+    public void heal (int h){
+        hp+=h;
+        if (hp > maxHP) hp = maxHP;
+    }
 
+    //The entity takes damage and lose HP
+    public void takeDamage(int d){
+        hp-=d;
+        if(hp < 0) hp = 0;
+    }
+
+    public String toString(){return name + " " + hp + "/" + maxHP;}
 }

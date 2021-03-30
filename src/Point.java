e22b79ca62c9fa9e5618357964a46b6b77f24521
quocngo-git <ngoquoc.work@gmail.com>
2021-03-30 16:43:00 -0700

    Update Point

--- src/Point.java
+++ src/Point.java
@@ -1,3 +1,15 @@
 public class Point {
-    
+    //Location x,y of an object in a 2D array
+    private int x;
+    private int y;
+
+    /**
+     * Location point x, y
+     * @param x location on x-axis
+     * @param y location on y-axis
+     */
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
 }

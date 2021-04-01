b12d7a033301c802d8a488e3c1c8da81e82276b4
quocngo-git <ngoquoc.work@gmail.com>
2021-04-01 13:46:19 -0700

    Create classes in Main

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,16 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Code starts");
     }
+
+    public static boolean monsterRoom(Hero h, Map m, EnemyGenerator eg, int level){
+        
+    }
+
+    public static void itemRoom(Hero h, Map m, ItemGenerator it){
+
+    }
+
+    public static boolean fight(Hero h, Enemy e){
+
+    }
 }

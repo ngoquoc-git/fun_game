f6f7de366a6db42460d902bd529624ea52bc9923
quocngo-git <ngoquoc.work@gmail.com>
2021-03-31 23:03:53 -0700

    LoadMap

--- src/Map.java
+++ src/Map.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class Map {
     private char[][] map;
     private boolean[][] revealed;
@@ -11,5 +14,31 @@ public class Map {
         revealed = new boolean[5][5];
     }
 
-    
+    /**
+     * Load map for each level
+     * Hidden map property with revealed = false
+     * @param mapNum 1, 2, or 3 from map's text files
+     */
+    public void loadMap(int mapNum){
+        try{
+            File readMapFile = new File("Map" + mapNum + ".txt");
+            Scanner sc = new Scanner(readMapFile);
+
+            while(sc.hasNextLine()){
+                for(int i = 0; i < map.length; i++){
+                    String mapLine = sc.nextLine().replace(" ", "");
+                    for(int j = 0; j < map.length; j++){
+                        map[i][j] = mapLine.charAt(j);
+                        revealed[i][j] = false;
+                    }
+                }
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println("File not found");
+            e.printStackTrace();
+        }
+    }
+
+
 }

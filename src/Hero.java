233a2589a92fe5688e962ba76569f1f1fdebf754
quocngo-git <ngoquoc.work@gmail.com>
2021-04-01 00:19:13 -0700

    Update abstract and interface functions

--- src/Hero.java
+++ src/Hero.java
@@ -1,3 +1,44 @@
-public class Hero {
+import java.util.Random;
+import java.util.ArrayList;
+public class Hero extends Entity implements Magical{
     
+    /**
+     * Magical Misslle skill
+     * @param e entity that is attacked
+     * @return attack action
+     */
+    @Override
+    public String magicalMisslle(Entity e){
+
+    }
+
+    /**
+     * Fire Ball skill
+     * @param e entity that is attacked
+     * @return attack action
+     */
+    @Override
+    public String fireBall(Entity e){
+
+    }
+
+    /**
+     * Thunderclap skill
+     * @param e entity that is attacked
+     * @return attack action
+     */
+    @Override
+    public String thunderClap(Entity e){
+
+    }
+
+    /**
+     * Abstract attack action that will be used by Heroes and Enemies
+     * @param e Entity that is attacked
+     * @return attack action
+     */
+    @Override
+    public String attack(Entity e){
+
+    }
 }

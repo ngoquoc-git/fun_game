e6aa2def69241ddde462ba5e2adce896ed0e9104
quocngo-git <ngoquoc.work@gmail.com>
2021-03-31 22:52:55 -0700

    Map constructor

--- src/Map.java
+++ src/Map.java
@@ -1,8 +1,15 @@
 public class Map {
     private char[][] map;
-    private boolean revealed;
+    private boolean[][] revealed;
 
+    /**
+     * Create a map sized 5x5
+     * Reveal trace
+     */
     public Map(){
-        
+        map= new char[5][5];
+        revealed = new boolean[5][5];
     }
+
+    
 }

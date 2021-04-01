60aa8d311e89d8d5256eea857fad5db153e6594a
quocngo-git <ngoquoc.work@gmail.com>
2021-04-01 13:51:34 -0700

    MonsterRoom and ItemRoom

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,26 @@
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Code starts");
     }
 
     public static boolean monsterRoom(Hero h, Map m, EnemyGenerator eg, int level){
-        
+        if (m.getCharAtLoc(h.getLocation()) == 'm'){
+            eg.generateEnemy(level);
+            return true;
+        }
+        else return false;
     }
 
-    public static void itemRoom(Hero h, Map m, ItemGenerator it){
-
+    public static void itemRoom(Hero h, Map m, ItemGenerator it) throws IOException{
+        if (m.getCharAtLoc(h.getLocation()) == 'i'){
+            h.pickUpItem(it.generateItem());
+            m.removeCharAtLoc(h.getLocation());
+        }
     }
 
     public static boolean fight(Hero h, Enemy e){
-
+        
     }
 }

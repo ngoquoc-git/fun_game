20ffcff6602bab31678ab66a30f92437e84befe3
quocngo-git <ngoquoc.work@gmail.com>
2021-04-01 00:10:55 -0700

    Update Magical

--- src/Magical.java
+++ src/Magical.java
@@ -1,3 +1,10 @@
 public interface Magical {
-    
+    public static final String MAGIC_MENU = "1.Magical Misslle.\n2. Fire Ball.\n3. Thunderclap.";
+
+    public String magicalMisslle(Entity e);
+
+    public String fireBall(Entity e);
+
+    public String thunderClap(Entity e);
+
 }
